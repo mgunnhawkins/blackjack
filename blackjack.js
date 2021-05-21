60b29fa19761c0044664de79e61a8bfb18bc9615
Melanie Gunn <melanie@melaniegunn.com>
2021-05-21 08:41:44 -0500

    finished create deck and moved to Deck Class

--- blackjack.js
+++ blackjack.js
@@ -1,51 +1,43 @@
 //TODO IMPORTS NEEDED
 const input = require('readline-sync');
 
-// class Card {
-//     constructor(suit, value){
-//         this.suit = suit
-//         this.rank = rank
-//         this.value = value
-//     }
-      
-//         //all face cards are worth 10
-//         //user will see suit and rank
-//         //total will use value instead of rank
-//         //ace is 1 or 11
-// }
-//     
+class Card {
+    constructor(suit, value){
+        this.suit = suit
+        this.rank = rank
+        this.value = value
+  }
 
-
-// class Deck{
-//     constructor(){
-//     //need looping structure
-//     //for each suit, 2 colors, 13 values
-//     this.cards = []
-//     }
+//user will see suit and rank
+//total will use value instead of rank
+//ace is 1 or 11
+   
+}
+  
+class Deck{
+    constructor(){
+        this.cards = []
+    }
+    createDeck() {
+        let cards = []
+        let suits = ["club", "heart", "spade", "diamond"]
+        let ranks = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+        let values = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+        suits.forEach(function(suit) {
+            for(let i=0; i < ranks.length; i++){
+                this.cards.push({suit:suit, rank:ranks[i], value:values[i]})
+            }
+        });
+        return this.cards 
+    }   
+}
   
     
-    //as I generate the cards it gets pushed into the deck
-    //deck = newDeck
+   
 
-function createDeck() {
-    let cards = []
-    let suits = ["club", "heart", "spade", "diamond"]
-    let ranks = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
-    let values = [1,2,3,4,5,6,7,8,9,10,10,10,10]
-    suits.forEach(function(suit) {
-        for(let i=0; i < ranks.length; i++){
-            cards.push({suit:suit, rank:ranks[i], value:values[i]})
-        }
-    });
-    return cards    
-}
-console.log(createDeck());
 
-// let cardValues = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
-// let clubs = new Cards('clubs', cardValues);
-// let hearts = new Cards('hearts', cardValues);
-// let spades = new Cards('spades', cardValues);
-// let diamonds = new Cards('diamonds', cardValues);clear
+
+
 
 
 // console.log(clubs, hearts, spades, diamonds);
