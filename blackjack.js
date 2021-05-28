06a0d7231663bd0044ff5b896601870fdf46b00c
Melanie Gunn <melanie@melaniegunn.com>
2021-05-28 14:55:59 -0500

    refactored deal function to return 1 card from randomized deck.

--- blackjack.js
+++ blackjack.js
@@ -1,89 +1,91 @@
 //TODO IMPORTS NEEDED
 const input = require('readline-sync');
-class Game{
-    
+class Game {
+
     //has
-    constructor(){
+    constructor() {
         this.deck = new Deck()
-        this.deal()
+        this.deck.shuffle()
+
+
+
         // this.players = players
         // this.total = total
     }
-    //do
-    //first deal method
-        //player gets 2 cards from deck
-            //cards get pushed into blank array named player hand
-            //cards get removed from deck
-            //print out player hand: (player hand array)
-            //print out total
-        //dealer gets 1 card from deck
-            //card gets pushed to empty array named dealer hand
-            //card gets removed from deck
-            //print out Dealer hand: (dealer hand array)
-            //print out total
-    deal(){
-        let randomNum = Math.floor(Math.random() * this.deck.cards.length)
-        console.log(randomNum)
-        let randomCards = this.deck.cards
-        //delete an item at a particular index OR shuffle
-    
 
+    dealCard() {
+        let card = this.deck.cards.pop();
+        console.log(card)
+            //return 1 card
     }
+
+    //print out player hand: (player hand array)
+    //print out total
+    //dealer gets 1 card from deck
+    //card gets pushed to empty array named dealer hand
+    //card gets removed from deck
+    //print out Dealer hand: (dealer hand array)
+    //print out total
 }
 
 
 
 class Card {
-    constructor(suit, value){
+    constructor(suit, value) {
         this.suit = suit
         this.rank = rank
         this.value = value
-  }
+    }
 
-//user will see suit and rank
-//total will use value instead of rank
-//ace is 1 or 11
-   
-}
-  
+    //user will see suit and rank
+    //total will use value instead of rank
+    //ace is 1 or 11
+
+} //end of card class
+
 class Deck {
-    constructor(){
+    constructor() {
         this.cards = []
         this.createDeck()
     }
     createDeck() {
         let cards = []
         let suits = ["club", "heart", "spade", "diamond"]
-        let ranks = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
-        let values = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+        let ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
         suits.forEach(function(suit) {
-            for(let i=0; i < ranks.length; i++){
-                cards.push({suit:suit, rank:ranks[i], value:values[i]})
+            for (let i = 0; i < ranks.length; i++) {
+                cards.push({ suit: suit, rank: ranks[i], value: values[i] })
             }
         });
         this.cards = cards
     }
-    shuffle(){
-        //google how to shuffle elements in an array in JS
-    }   
+    shuffle() {
+        this.cards = this.cards.sort(() => Math.random() - 0.5)
+    }
+} //end of Deck Class
+
+class Player {
+
 }
 
-    
-let playGame = new Game()
-
-
-    
-    //hand
-    //total   
-    //hit method
-    //stay method
+let game = new Game()
+game.dealCard();
 
 
 
 
-  
-    
-   
+//hand
+//total   
+//hit method
+//stay method
+
+
+
+
+
+
+
 
 
 
@@ -96,7 +98,7 @@ let playGame = new Game()
 
 // let chooseCards() {
 // }  
-    
+
 // runProgram(){
 //     let playerChoice = input.question("Hit or stay?")
 //     console.log (playerChoice)
@@ -107,14 +109,13 @@ let playGame = new Game()
 //total of player's hand needed each time
 
 // Each player starts with two cards, one of the dealer's cards is hidden until the end.
-    //array needs to pull 2 cards out of the array and give to the user
+//array needs to pull 2 cards out of the array and give to the user
 
 // To 'Hit' is to ask for another card. To 'Stand' is to hold your total and end your turn.
-    //prompt user to write hit or stand
-    //if hit then pull out another card out of the deck and push to the player.  Then prompt user again for hit or stand
-    //if stand then leave alone
+//prompt user to write hit or stand
+//if hit then pull out another card out of the deck and push to the player.  Then prompt user again for hit or stand
+//if stand then leave alone
 
 // If you go over 21 you bust, and the dealer wins regardless of the dealer's hand.
-    //if over 21 then bust
+//if over 21 then bust
 // If you are dealt 21 from the start (Ace & 10), you got a blackjack.
-
